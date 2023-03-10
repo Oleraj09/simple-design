f130c7818c6ec4251776f619daf19bb76e38eb45
Oleraj <olerajhossin@gmail.com>
2023-03-10 19:06:37 +0600

    Animation with CSS

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('.hero-image', {
+    delay: 1000,
+    loop: true,
+    origin: "right",
+    distance: "200px",
+    easing: "ease-in-out"
+});
+ScrollReveal().reveal('.num1', {
+    delay: 1000,
+    loop: true,
+    origin: "bottom",
+    distance: "50px",
+    easing: "ease-in-out"
+});
+ScrollReveal().reveal('.num2', {
+    delay: 1500,
+    loop: true,
+    origin: "bottom",
+    distance: "50px",
+    easing: "ease-in-out"
+});
+ScrollReveal().reveal('.num3', {
+    delay: 2000,
+    loop: true,
+    origin: "bottom",
+    distance: "50px",
+    easing: "ease-in-out"
+});
+
+new CircleType(document.getElementById('curved'))
+  .radius(300);
+
